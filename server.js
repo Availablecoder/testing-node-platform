0b0a7bea38c8549d04edf11622464d17ffb7cc00
Availablecoder <ahmed.cross20@gmail.com>
2022-08-30 11:45:05 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+const hostname = "0.0.0.0";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Testing Zeet platform");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server is running ar port ${port}`);
+});
